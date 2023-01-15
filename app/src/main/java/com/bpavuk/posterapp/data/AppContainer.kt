284ea5e1b915dbe29942120b8dcbbffa91d9012f
bpavuk <boopentiy@gmail.com>
2023-01-15 23:45:26 +0200

    Using localhost mock server instead of a Beeceptor mocking service

--- app/src/main/java/com/bpavuk/posterapp/data/AppContainer.kt
+++ app/src/main/java/com/bpavuk/posterapp/data/AppContainer.kt
@@ -12,7 +12,7 @@ interface AppContainer {
 }
 
 class MockedApiAppContainer: AppContainer {
-    private val baseUrl = "https://poster.free.beeceptor.com/"
+    private val baseUrl = "https://127.0.0.1:3001/"
 
     @OptIn(ExperimentalSerializationApi::class)
     private val retrofit = Retrofit.Builder()
