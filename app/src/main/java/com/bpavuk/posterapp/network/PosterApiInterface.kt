5aa8b6adf58732bf6fd5c47a95685e7c542de310
bpavuk <boopentiy@gmail.com>
2023-01-19 01:49:33 +0200

    API now able to get users by ID. #4

--- app/src/main/java/com/bpavuk/posterapp/network/PosterApiInterface.kt
+++ app/src/main/java/com/bpavuk/posterapp/network/PosterApiInterface.kt
@@ -3,6 +3,7 @@ package com.bpavuk.posterapp.network
 import com.bpavuk.posterapp.model.Post
 import com.bpavuk.posterapp.model.User
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PosterApiInterface {
@@ -11,5 +12,6 @@ interface PosterApiInterface {
         @Query("from") lastPostId: Int,
         @Query("limit") posts: Int
     ): List<Post>
-    suspend fun getUserById(userId: Int): User
+    @GET("user/{id}")
+    suspend fun getUserById(@Path("id") userId: Int): User
 }
