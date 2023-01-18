0a78c28a1adbacd1b58131d9eae6c7263fd038e4
bpavuk <boopentiy@gmail.com>
2023-01-19 01:48:57 +0200

    Post model prepared to host author. #4

--- app/src/main/java/com/bpavuk/posterapp/model/Post.kt
+++ app/src/main/java/com/bpavuk/posterapp/model/Post.kt
@@ -2,11 +2,13 @@ package com.bpavuk.posterapp.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class Post(
     val id: Int,
     @SerialName("author_id") val authorId: Int,
     val text: String? = null,
-    @SerialName("img_url") val imgURL: String
+    @SerialName("img_url") val imgURL: String,
+    @Transient var author: User? = null,
 )
