7716aac4ef2ff98d6c156b6a3ea165bcb5bb609e
bpavuk <boopentiy@gmail.com>
2023-01-19 15:32:54 +0200

    Implemented FakePosterApiInterface.getUserById function for tests

--- app/src/test/java/com/bpavuk/posterapp/ui/test/fake/FakePosterApiInterface.kt
+++ app/src/test/java/com/bpavuk/posterapp/ui/test/fake/FakePosterApiInterface.kt
@@ -6,7 +6,5 @@ import com.bpavuk.posterapp.network.PosterApiInterface
 
 object FakePosterApiInterface: PosterApiInterface {
     override suspend fun getPosts(lastPostId: Int, posts: Int): List<Post> = Datasource.fakePosts
-    override suspend fun getUserById(userId: Int): User {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getUserById(userId: Int): User = Datasource.users.first { userId == it.id }
 }
