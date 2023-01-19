f40848282e9b8c63cd14c898ba2ec987b113ea75
bpavuk <boopentiy@gmail.com>
2023-01-19 15:32:10 +0200

    Added users in fake Datasource.kt

--- app/src/test/java/com/bpavuk/posterapp/ui/test/fake/Datasource.kt
+++ app/src/test/java/com/bpavuk/posterapp/ui/test/fake/Datasource.kt
@@ -1,6 +1,7 @@
 package com.bpavuk.posterapp.ui.test.fake
 
 import com.bpavuk.posterapp.model.Post
+import com.bpavuk.posterapp.model.User
 
 
 object Datasource {
@@ -29,5 +30,23 @@ object Datasource {
             text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             imgURL = "https://picsum.photos/seed/kittens/${(400..1920).random()}/${(400..1920).random()}"
         )
+    )
+
+    val users = listOf(
+        User(
+            id = 0,
+            userName = "beckie228",
+            profileImgUrl = "sample img url"
+        ),
+        User(
+            id = 1,
+            userName = "enchanted_ant",
+            profileImgUrl = "sample img url"
+        ),
+        User(
+            id = 2,
+            userName = "favey_inSauce",
+            profileImgUrl = "sample img url"
+        ),
     )
 }
