66b97da0dd4494dd79d3c27267e39a5c38af8309
bpavuk <boopentiy@gmail.com>
2023-01-20 19:14:51 +0200

    Updated model accordingly to updates in API

--- app/src/main/java/com/bpavuk/posterapp/model/Post.kt
+++ app/src/main/java/com/bpavuk/posterapp/model/Post.kt
@@ -2,13 +2,11 @@ package com.bpavuk.posterapp.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 
 @Serializable
 data class Post(
     val id: Int,
-    @SerialName("author_id") val authorId: Int,
     val text: String? = null,
     @SerialName("img_url") val imgURL: String,
-    @Transient var author: User? = null,
+    var author: User
 )
