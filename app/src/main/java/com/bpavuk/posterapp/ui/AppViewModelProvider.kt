7d2b57e9c6f74af78e9082188d4aab3fb89f15ab
bpavuk <boopentiy@gmail.com>
2023-02-01 20:33:08 +0200

    Added new viewmodel to provider

--- app/src/main/java/com/bpavuk/posterapp/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/AppViewModelProvider.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bpavuk.posterapp.PosterApplication
+import com.bpavuk.posterapp.ui.common.LoginScreenViewModel
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
         initializer {
             PosterAppViewModel(application().container.defaultPosterRepository)
+        }
+        initializer {
+            LoginScreenViewModel(application().container.defaultPosterRepository)
         }
     }
 }
