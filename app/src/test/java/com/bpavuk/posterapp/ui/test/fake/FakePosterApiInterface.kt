76c40db7203a8fd759e557256d48b70b4bfba79e
bpavuk <boopentiy@gmail.com>
2023-02-01 20:26:21 +0200

    Updated fakes accordingly to previous commits

--- app/src/test/java/com/bpavuk/posterapp/ui/test/fake/FakePosterApiInterface.kt
+++ app/src/test/java/com/bpavuk/posterapp/ui/test/fake/FakePosterApiInterface.kt
@@ -1,5 +1,6 @@
 package com.bpavuk.posterapp.ui.test.fake
 
+import com.bpavuk.posterapp.model.AuthResponse
 import com.bpavuk.posterapp.model.Post
 import com.bpavuk.posterapp.model.User
 import com.bpavuk.posterapp.network.PosterApiInterface
@@ -7,4 +8,15 @@ import com.bpavuk.posterapp.network.PosterApiInterface
 object FakePosterApiInterface: PosterApiInterface {
     override suspend fun getPosts(lastPostId: Int, posts: Int): List<Post> = Datasource.fakePosts
     override suspend fun getUserById(userId: Int): User = Datasource.users.first { userId == it.id }
+    override suspend fun getToken(
+        grantType: String,
+        username: String,
+        password: String
+    ): AuthResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getMe(token: String): User {
+        TODO("Not yet implemented")
+    }
 }
