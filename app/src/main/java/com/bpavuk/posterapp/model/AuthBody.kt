dd99f97626d6971e2cb3c778603720dced683e04
bpavuk <boopentiy@gmail.com>
2023-02-01 20:21:07 +0200

    Updated AuthBody model for full support of OAuth2 specification

--- app/src/main/java/com/bpavuk/posterapp/model/AuthBody.kt
+++ app/src/main/java/com/bpavuk/posterapp/model/AuthBody.kt
@@ -5,5 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class AuthBody(
     val username: String,
-    val password: String
+    val password: String,
+    val grant_type: String = "",
+    val scope: String = "",
+    val client_id: String = "",
+    val client_secret: String = ""
 )
