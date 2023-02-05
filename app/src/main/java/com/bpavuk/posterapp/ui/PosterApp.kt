bb5e61a5a0c28c0c7cbc704436ccd6eafce3f18d
bpavuk <boopentiy@gmail.com>
2023-02-05 20:10:17 +0200

    Updated PosterApp composable accordingly to previous commit

--- app/src/main/java/com/bpavuk/posterapp/ui/PosterApp.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/PosterApp.kt
@@ -1,32 +1,17 @@
 package com.bpavuk.posterapp.ui
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.bpavuk.posterapp.ui.common.PostCardsList
+import com.bpavuk.posterapp.ui.common.PostCardsListScreen
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PosterApp(
     windowWidthSizeClass: WindowWidthSizeClass,
     modifier: Modifier = Modifier
 ) {
-    val viewModel: PosterAppViewModel = viewModel(factory = AppViewModelProvider.Factory)
-    val uiState = viewModel.uiState
-
-    Scaffold { paddingValues ->
-        PostCardsList(
-            uiState = uiState,
-            windowWidthSizeClass = windowWidthSizeClass,
-            onReachedEnd = { viewModel.updatePosts(it) },
-            modifier = modifier
-                .padding(paddingValues)
-                .fillMaxWidth()
-        )
-    }
+    PostCardsListScreen(
+        windowWidthSizeClass = windowWidthSizeClass,
+        modifier = modifier
+    )
 }
