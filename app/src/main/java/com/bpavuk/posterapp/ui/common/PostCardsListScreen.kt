1ee4f9f44c49852e949d8eb7c5570082f2b362d6
bpavuk <boopentiy@gmail.com>
2023-02-05 20:09:29 +0200

    Moved screen with list of posts to separate composable

---
+++ app/src/main/java/com/bpavuk/posterapp/ui/common/PostCardsListScreen.kt
@@ -0,0 +1,29 @@
+package com.bpavuk.posterapp.ui.common
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bpavuk.posterapp.ui.AppViewModelProvider
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PostCardsListScreen(windowWidthSizeClass: WindowWidthSizeClass, modifier: Modifier = Modifier) {
+    val viewModel: PostCardsListViewModel = viewModel(factory = AppViewModelProvider.Factory)
+    val uiState = viewModel.uiState
+
+    Scaffold { paddingValues ->
+        PostCardsList(
+            uiState = uiState,
+            windowWidthSizeClass = windowWidthSizeClass,
+            onReachedEnd = { viewModel.updatePosts(it) },
+            modifier = modifier
+                .padding(paddingValues)
+                .fillMaxWidth()
+        )
+    }
+}
