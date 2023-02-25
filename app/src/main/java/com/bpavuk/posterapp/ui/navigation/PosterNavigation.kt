ea174caa96dcd1bb69dd201a279a41a1c25f42bd
bpavuk <boopentiy@gmail.com>
2023-02-25 17:33:13 +0200

    Improved PosterNavigation class

--- app/src/main/java/com/bpavuk/posterapp/ui/navigation/PosterNavigation.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/navigation/PosterNavigation.kt
@@ -1,5 +1,13 @@
 package com.bpavuk.posterapp.ui.navigation
 
-enum class PosterNavigation {
-    HOME, ME
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.bpavuk.posterapp.R
+
+enum class PosterNavigation(@StringRes val string: Int, val icon: ImageVector) {
+    HOME(R.string.home, icon = Icons.Outlined.Home),
+    ME(R.string.me, icon = Icons.Outlined.AccountBox)
 }
