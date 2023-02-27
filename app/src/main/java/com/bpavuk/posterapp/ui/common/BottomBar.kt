51eedfd99ddfcfac4a8c691792d34b962f523048
bpavuk <boopentiy@gmail.com>
2023-02-27 16:10:35 +0200

    Bottom bar now showing what the screen you are on

--- app/src/main/java/com/bpavuk/posterapp/ui/common/BottomBar.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/common/BottomBar.kt
@@ -1,45 +1,86 @@
 package com.bpavuk.posterapp.ui.common
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bpavuk.posterapp.ui.navigation.PosterNavigation
 
 @Composable
-fun BottomAppBar(onClick: (PosterNavigation) -> Unit = {}) {
+fun BottomAppBar(
+    onClick: (PosterNavigation) -> Unit = {},
+    activeButton: PosterNavigation
+) {
     NavigationBar {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(72.dp)
-                .background(color = MaterialTheme.colorScheme.tertiaryContainer)
+                .background(color = MaterialTheme.colorScheme.surface)
                 .wrapContentSize(align = Alignment.Center)
         ) {
-            Spacer(modifier = Modifier.weight(1f))
             for (i in PosterNavigation.values()) {
-                IconButton(onClick = { onClick(i) }) {
-                    AppBarIcon(icon = i)
+                IconButton(
+                    onClick = { onClick(i) },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    interactionSource = NoRippleInteractionSource()
+                ) {
+                    AppBarIcon(
+                        icon = i,
+                        isActive = activeButton == i,
+                        modifier = Modifier
+                            .defaultMinSize(minWidth = 32.dp)
+                            .fillMaxHeight()
+                    )
                 }
-                Spacer(modifier = Modifier.weight(1f))
             }
         }
     }
 }
 
 @Composable
-fun AppBarIcon(icon: PosterNavigation) {
-    Column {
-        Icon(
-            imageVector = icon.icon,
-            contentDescription = stringResource(id = icon.string),
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
+fun AppBarIcon(
+    icon: PosterNavigation,
+    isActive: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val buttonWidth by animateDpAsState(
+        targetValue = if (isActive) 60.dp else 24.dp,
+        animationSpec = tween(durationMillis = 200, easing = FastOutSlowInEasing)
+    )
+
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
+        Box(
+            modifier = Modifier
+                .width(buttonWidth)
+                .align(Alignment.CenterHorizontally)
+                .clip(shape = MaterialTheme.shapes.extraLarge)
+                .background(
+                    color = if (isActive) {
+                        MaterialTheme.colorScheme.secondaryContainer
+                    } else {
+                        MaterialTheme.colorScheme.surface
+                    }
+                )
+        ) {
+            Icon(
+                imageVector = icon.icon,
+                contentDescription = stringResource(id = icon.string),
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
         Text(
             text = stringResource(id = icon.string),
             modifier = Modifier.align(Alignment.CenterHorizontally)
@@ -50,5 +91,5 @@ fun AppBarIcon(icon: PosterNavigation) {
 @Preview
 @Composable
 fun BottomAppBarPreview() {
-    BottomAppBar()
+    BottomAppBar(activeButton = PosterNavigation.HOME)
 }
---
+++ app/src/main/java/com/bpavuk/posterapp/ui/common/NoRippleInteractionSource.kt
@@ -0,0 +1,18 @@
+package com.bpavuk.posterapp.ui.common
+
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+/**
+* Used for disabling ripple effect in some buttons
+ */
+class NoRippleInteractionSource : MutableInteractionSource {
+
+    override val interactions: Flow<Interaction> = emptyFlow()
+
+    override suspend fun emit(interaction: Interaction) {}
+
+    override fun tryEmit(interaction: Interaction) = true
+}
