ce3ebff8d7f00cf5437649fa83b25fb7429ea331
bpavuk <boopentiy@gmail.com>
2023-03-03 18:21:37 +0200

    Updated fakes

--- app/src/test/java/com/bpavuk/posterapp/ui/test/fake/FakePosterRepository.kt
+++ app/src/test/java/com/bpavuk/posterapp/ui/test/fake/FakePosterRepository.kt
@@ -3,12 +3,15 @@ package com.bpavuk.posterapp.ui.test.fake
 import com.bpavuk.posterapp.data.PosterRepository
 import com.bpavuk.posterapp.model.AuthBody
 import com.bpavuk.posterapp.model.AuthResponse
-import com.bpavuk.posterapp.network.PosterApiInterface
 import com.bpavuk.posterapp.model.Post
 import com.bpavuk.posterapp.model.User
+import com.bpavuk.posterapp.network.PosterApiInterface
 
 class FakePosterRepository(private val posterApiInterface: PosterApiInterface) : PosterRepository {
-    override suspend fun getOnlinePosts(lastPostId: Int): List<Post> = posterApiInterface.getPosts(lastPostId, 5)
+    override suspend fun getOnlinePosts(lastPostId: Int): List<Post> = posterApiInterface.getPosts(
+        lastPostId,
+        5
+    )
     override suspend fun getUser(userId: Int): User = posterApiInterface.getUserById(userId)
     override suspend fun getToken(authBody: AuthBody): AuthResponse =
         posterApiInterface.getToken(
@@ -18,4 +21,7 @@ class FakePosterRepository(private val posterApiInterface: PosterApiInterface) :
 
     override suspend fun getMe(token: String): User =
         posterApiInterface.getMe(token)
+
+    override suspend fun getPostsByUser(lastPostId: Int, user: User): List<Post> =
+        posterApiInterface.getPosts(lastPostId, posts = 12, username = user.userName)
 }
