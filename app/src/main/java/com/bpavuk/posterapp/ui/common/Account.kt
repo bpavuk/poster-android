3721a4566c5eab61ff71600fb3cbfa851fb30d42
bpavuk <boopentiy@gmail.com>
2023-03-04 17:47:41 +0200

    Added PostsGrid composable, removed AccountPreview

--- app/src/main/java/com/bpavuk/posterapp/ui/common/Account.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/common/Account.kt
@@ -1,18 +1,29 @@
 package com.bpavuk.posterapp.ui.common
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.bpavuk.posterapp.model.Post
 import com.bpavuk.posterapp.model.User
-import com.bpavuk.posterapp.ui.theme.PosterTheme
 
 @Composable
 fun AccountPhoto(
@@ -58,11 +69,27 @@ fun AccountCard(user: User) {
     }
 }
 
-@Preview
 @Composable
-fun AccountPreview() {
-    val fakeUser = User(userName = "fuckery", id = 10, profileImgUrl = "https://picsum.photos/200/300")
-    PosterTheme {
-        AccountCard(user = fakeUser)
+fun PostsGrid(
+    postsList: List<Post>,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(96.dp),
+        modifier = modifier
+    ) {
+        items(postsList) {post ->
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(post.imgURL)
+                    .build(),
+                contentDescription = post.author.userName,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+            )
+        }
     }
 }
+
