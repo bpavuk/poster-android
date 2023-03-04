01e7aacaf3ddb395ea771df84d520f99da5b3a1d
bpavuk <boopentiy@gmail.com>
2023-03-04 17:14:33 +0200

    Updated PosterRepository accordingly to previous commit

--- app/src/main/java/com/bpavuk/posterapp/data/PosterRepository.kt
+++ app/src/main/java/com/bpavuk/posterapp/data/PosterRepository.kt
@@ -7,18 +7,18 @@ import com.bpavuk.posterapp.model.User
 import com.bpavuk.posterapp.network.PosterApiInterface
 
 interface PosterRepository {
-    suspend fun getOnlinePosts(lastPostId: Int): List<Post>
+    suspend fun getOnlinePosts(lastPostId: Int): List<Post>?
     suspend fun getUser(userId: Int): User
 
     suspend fun getToken(authBody: AuthBody): AuthResponse
 
     suspend fun getMe(token: String): User
 
-    suspend fun getPostsByUser(lastPostId: Int, user: User): List<Post>
+    suspend fun getPostsByUser(lastPostId: Int, user: User): List<Post>?
 }
 
 class DefaultPosterRepository(private val posterApiInterface: PosterApiInterface): PosterRepository {
-    override suspend fun getOnlinePosts(lastPostId: Int): List<Post> =
+    override suspend fun getOnlinePosts(lastPostId: Int): List<Post>? =
         posterApiInterface.getPosts(lastPostId, 3)
 
     override suspend fun getUser(userId: Int) =
@@ -30,6 +30,6 @@ class DefaultPosterRepository(private val posterApiInterface: PosterApiInterface
     override suspend fun getMe(token: String): User =
         posterApiInterface.getMe("Bearer $token")
 
-    override suspend fun getPostsByUser(lastPostId: Int, user: User): List<Post> =
+    override suspend fun getPostsByUser(lastPostId: Int, user: User): List<Post>? =
         posterApiInterface.getPosts(lastPostId = lastPostId, posts = 12, username = user.userName)
 }
