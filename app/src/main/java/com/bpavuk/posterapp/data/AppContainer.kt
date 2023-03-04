f51fdf22834ca19e028de5b0ab1a22a0a4eebe50
bpavuk <boopentiy@gmail.com>
2023-03-04 17:15:42 +0200

    Using Json instead of Json5

--- app/src/main/java/com/bpavuk/posterapp/data/AppContainer.kt
+++ app/src/main/java/com/bpavuk/posterapp/data/AppContainer.kt
@@ -4,8 +4,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import com.bpavuk.posterapp.network.PosterApiInterface
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import io.github.xn32.json5k.Json5
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
@@ -17,7 +17,10 @@ interface AppContainer {
 class MockedApiAppContainer(dataStore: DataStore<Preferences>): AppContainer {
     private val baseUrl = "https://127.0.0.1:3001/" // Poster API
 
-    private val json = Json5 { prettyPrint = true }
+    private val json = Json {
+        isLenient = true
+        coerceInputValues = true
+    }
 
     @OptIn(ExperimentalSerializationApi::class)
     private val retrofit = Retrofit.Builder()
