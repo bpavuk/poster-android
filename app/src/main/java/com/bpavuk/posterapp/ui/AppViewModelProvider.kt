66af2dfb2c564e2604f09b8a1df1f91a6d3c79c1
bpavuk <boopentiy@gmail.com>
2023-03-04 21:40:17 +0200

    Added new viewmodel to AppViewModelProvider

--- app/src/main/java/com/bpavuk/posterapp/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/AppViewModelProvider.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bpavuk.posterapp.PosterApplication
+import com.bpavuk.posterapp.ui.common.viewmodels.AccountScreenViewModel
 import com.bpavuk.posterapp.ui.common.viewmodels.LoginScreenViewModel
 import com.bpavuk.posterapp.ui.common.viewmodels.PostCardsListViewModel
 import com.bpavuk.posterapp.ui.common.viewmodels.PosterAppViewModel
@@ -21,6 +22,12 @@ object AppViewModelProvider {
             )
         }
         initializer { PosterAppViewModel() }
+        initializer {
+            AccountScreenViewModel(
+                application().container.defaultPosterRepository,
+                application().container.defaultAuthenticationRepository
+            )
+        }
     }
 }
 
