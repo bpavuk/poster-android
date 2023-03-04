c461aa22a77c603b91947f70e1a068e47519b244
bpavuk <boopentiy@gmail.com>
2023-03-04 21:39:40 +0200

    Added AccountScreen

--- app/src/main/java/com/bpavuk/posterapp/ui/common/Account.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/common/Account.kt
@@ -1,8 +1,10 @@
 package com.bpavuk.posterapp.ui.common
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -19,11 +21,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.bpavuk.posterapp.R
 import com.bpavuk.posterapp.model.Post
 import com.bpavuk.posterapp.model.User
+import com.bpavuk.posterapp.ui.AppViewModelProvider
+import com.bpavuk.posterapp.ui.common.viewmodels.AccountScreenViewModel
+import com.bpavuk.posterapp.ui.common.viewmodels.UiState
+import com.bpavuk.posterapp.ui.theme.PosterTheme
 
 @Composable
 fun AccountPhoto(
@@ -93,3 +103,39 @@ fun PostsGrid(
     }
 }
 
+@Composable
+fun AccountScreen(
+    viewModel: AccountScreenViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+
+    when (val uiState = viewModel.uiState) {
+        is UiState.Success -> {
+            Column {
+                AccountCard(user = uiState.user)
+                PostsGrid(postsList = uiState.postsList ?: emptyList())
+            }
+        }
+        is UiState.Error -> {
+            when (uiState.code) {
+                401 -> LoginScreen()
+                else -> Text(text = stringResource(id = R.string.internet_oops))
+            }
+        }
+        UiState.Loading -> {
+            Text(text = stringResource(id = R.string.loading))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AccountsPreview() {
+    PosterTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AccountScreen()
+        }
+    }
+}
