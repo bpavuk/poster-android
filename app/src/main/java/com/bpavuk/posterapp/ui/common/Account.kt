6759bd91ee1612a601caa287ae53d87e091770e7
bpavuk <boopentiy@gmail.com>
2023-03-05 15:08:44 +0200

    Implemented jetpack navigation, moved actual profile screen to separate composable

--- app/src/main/java/com/bpavuk/posterapp/ui/common/Account.kt
+++ app/src/main/java/com/bpavuk/posterapp/ui/common/Account.kt
@@ -1,10 +1,10 @@
 package com.bpavuk.posterapp.ui.common
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -17,14 +17,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.bpavuk.posterapp.R
@@ -33,7 +37,6 @@ import com.bpavuk.posterapp.model.User
 import com.bpavuk.posterapp.ui.AppViewModelProvider
 import com.bpavuk.posterapp.ui.common.viewmodels.AccountScreenViewModel
 import com.bpavuk.posterapp.ui.common.viewmodels.UiState
-import com.bpavuk.posterapp.ui.theme.PosterTheme
 
 @Composable
 fun AccountPhoto(
@@ -104,25 +107,58 @@ fun PostsGrid(
 }
 
 @Composable
+fun ProfileScreen(
+    uiState: UiState,
+    navController: NavHostController
+) {
+    Log.d("login", "Account screen returns to login")
+    Column {
+        when (uiState) {
+            is UiState.Success -> {
+                AccountCard(user = uiState.user)
+                PostsGrid(
+                    postsList = uiState.postsList ?: emptyList(),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(MaterialTheme.shapes.extraLarge)
+                )
+            }
+            is UiState.Error -> {
+                when (uiState.code) {
+                    401, 422 -> LaunchedEffect(key1 = 401) {
+                        navController.navigate(route = "login")
+                    }
+                    else -> {
+                        Text(text = stringResource(id = R.string.internet_oops))
+                        Log.d("login", uiState.code.toString())
+                    }
+                }
+            }
+            UiState.Loading -> Text(text = stringResource(id = R.string.loading))
+        }
+    }
+}
+
+@Composable
 fun AccountScreen(
     viewModel: AccountScreenViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val navController = rememberNavController()
+    val uiState = viewModel.uiState
 
-    when (val uiState = viewModel.uiState) {
-        is UiState.Success -> {
-            Column {
-                AccountCard(user = uiState.user)
-                PostsGrid(postsList = uiState.postsList ?: emptyList())
-            }
+    NavHost(navController = navController, startDestination = "account") {
+        composable(route = "account") {
+            ProfileScreen(uiState = uiState, navController = navController)
         }
-        is UiState.Error -> {
-            when (uiState.code) {
-                401 -> LoginScreen()
-                else -> Text(text = stringResource(id = R.string.internet_oops))
-            }
-        }
-        UiState.Loading -> {
-            Text(text = stringResource(id = R.string.loading))
+        composable(route = "login") {
+            LoginScreen(onSuccessfulLogin = {
+                navController.popBackStack(
+                    route = "account",
+                    inclusive = false,
+                    saveState = true
+                )
+                viewModel.updateInfo()
+            })
         }
     }
 }
